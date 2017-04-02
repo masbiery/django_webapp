bb1314d6768ad7bee8124fdf29d7862844e9fc5b
Mbiery <masbiery@gmail.com>
2017-04-02 19:54:20 +0000

    added database for the page count.

--- helloworld/models.py
+++ helloworld/models.py
@@ -3,3 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class PageCount(models.Model):
+    count = models.IntegerField(default=0)
+    page = models.URLField()
