75f2588d117bc19fb1e02d92de9bc29bc5566d38
Mbiery <masbiery@gmail.com>
2017-04-02 20:27:10 +0000

    Page counter has been added.

--- helloworld/views.py
+++ helloworld/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import time
 from ipware.ip import get_real_ip
+from .models import PageCount
 
 def index(request):
+    pageCount, create = PageCount.objects.get_or_create(page='index')
+    pageCount.count += 1
+    pageCount.save()
     ip = (get_real_ip(request))
-    return HttpResponse("Helloworld!   " + time.strftime("%c") + "---Your IP Address is " + ip)
+    return HttpResponse("Helloworld!   " + time.strftime("%c") + "   ---Your IP Address is " + ip + "   ---Page count is " + str(pageCount.count))
