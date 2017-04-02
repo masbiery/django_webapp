7233b7c6b897e1012c56dce4d54f5fea36c27318
Mbiery <masbiery@gmail.com>
2017-04-02 19:30:37 +0000

    Added dynamic ip address

--- helloworld/views.py
+++ helloworld/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import time
+from ipware.ip import get_real_ip
 
 def index(request):
-    return HttpResponse("Helloworld!   " + time.strftime("%c"))
+    ip = (get_real_ip(request))
+    return HttpResponse("Helloworld!   " + time.strftime("%c") + "---Your IP Address is " + ip)
